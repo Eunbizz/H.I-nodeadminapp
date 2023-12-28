e3974195f500c2d34d9abe53932edb1fe822e181
boram <boram910530@gmail.com>
2023-12-29 00:07:21 +0900

    admin 충돌 머지

--- routes/index.js
+++ routes/index.js
@@ -57,7 +57,7 @@ router.get('/register', async(req, res)=>{
     res.render('login/register', {layout:"loginLayout"});
 });
 
-router.get('/register', async(req, res)=>{
+router.post('/register', async(req, res)=>{
     res.redirect('/login');
 });
 

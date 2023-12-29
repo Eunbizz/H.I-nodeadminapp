6679270fed346f630d307240f0d77988c067a007
boram <boram910530@gmail.com>
2023-12-29 10:30:22 +0900

    로그인 name 수정

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,14 @@ router.get('/', function(req, res) {
 });
 
 router.get('/login', async(req, res)=>{
-    res.render('login',{resultMsg:"", id:"", password:"", layout:"loginLayout"});
+    res.render('login',{resultMsg:"", id:"", pw:"", layout:"loginLayout"});
 });
 
 router.post('/login', async(req, res)=>{
 
     // 사용자 로그인정보 추출
     var id = req.body.id; 
-    var password = req.body.password;   
+    var pw = req.body.pw;   
 
     // DB admin 테이블에서 동일한 메일주소의 단일사용자 정보를 조회한다.
     var admin = await db.Admin.findOne({where:{admin_id:id}});
@@ -32,7 +32,7 @@ router.post('/login', async(req, res)=>{
         resultMsg = '관리자 정보가 등록되지 않았습니다.'
     } else {
         // 입력한 패스워드가 db패스워드와 같을 때 메인페이지로 이동
-        if(admin.password == password) {
+        if(admin.admin_password == pw) {
             res.redirect('/');
         } else {
             resultMsg = '암호가 일치하지 않습니다.'
@@ -40,7 +40,7 @@ router.post('/login', async(req, res)=>{
     }
 
     if(resultMsg !=='') {
-        res.render('login', {resultMsg, id, password, layout:"loginLayout"})
+        res.render('login', {resultMsg, id, pw, layout:"loginLayout"})
         // res.redirect('/login');
     }
 });
